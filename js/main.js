d5041410df76e6df7c116e829d1d9c0d8016075c
nocoded <josh@nocoded.com>
2024-03-29 13:33:05 -0600

    Added numeric and date validation and comparison functionality

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
 
     // sort table data:
     $(document).on("click", "table thead tr th:not(.no-sort)", function() {
-        var table = $(this).parents("table");
-        var rows  = $(this).parents("table").find("tbody tr").toArray().sort(TableComparer($(this).index()));
-        var dir   = ($(this).hasClass("sort-asc")) ? "desc" : "asc";
+        let table = $(this).parents("table");
+        let rows  = $(this).parents("table").find("tbody tr").toArray().sort(TableComparer($(this).index()));
+        let dir   = ($(this).hasClass("sort-asc")) ? "desc" : "asc";
 
         if (dir == "desc") {
             rows = rows.reverse();
         }
 
-        for (var i = 0; i < rows.length; i++) {
+        for (let i = 0; i < rows.length; i++) {
             table.append(rows[i]);
         }
 
@@ -24,11 +24,22 @@ $(document).ready(function() {
 
 // table data comparison:
 function TableComparer(index) {
-    return function(a, b) {
-        var val_a  = TableCellValue(a, index);
-        var val_b  = TableCellValue(b, index);
-        var result = ($.isNumeric(val_a) && $.isNumeric(val_b)) ? val_a - val_b : val_a.toString().localeCompare(val_b);
-
+    return function (a, b) {
+        
+        let val_a = TableCellValue(a, index).replace(/\$\,/g, "");
+        let val_b = TableCellValue(b, index).replace(/\$\,/g, "");
+        let result = val_a.toString().localeCompare(val_b);
+        
+        if ($.isNumeric(val_a) && $.isNumeric(val_b)) {
+            result = val_a - val_b;
+        }
+        
+        if (isDate(val_a) && isDate(val_b)) {
+            let date_a = new Date(val_a); 
+            let date_b = new Date(val_b);
+            result = date_a - date_b;
+        }
+        
         return result;
     }
 }
@@ -37,4 +48,12 @@ function TableComparer(index) {
 // get table cell value:
 function TableCellValue(row, index) {
     return $(row).children("td").eq(index).text();
+}
+
+
+// date validation:
+function isDate(val) {
+    let d = new Date(val);
+    
+    return !isNaN(d.valueOf());
 }
